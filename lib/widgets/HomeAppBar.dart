be85f540d458fedfef8b3ad616e2df31c5bddf21
lionel3107 <dlmsonelterror@gmail.com>
2024-06-04 15:39:47 +0000

    missed the card button due to the uninstalled packge

--- lib/widgets/HomeAppBar.dart
+++ lib/widgets/HomeAppBar.dart
@@ -1,19 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart' as badges;
 
 class HomeAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color : Colors.white,
+      color: Colors.white,
       padding: EdgeInsets.all(25),
-      child: Row(
+      child: const Row(
         children: [
           Icon(
             Icons.sort,
             size: 30,
             color: Color(0xFF4C53A5),
           ),
-          
+          Padding(
+            padding: EdgeInsets.only(
+              left: 20,
+            ),
+            child: Text(
+              "Db Shop",
+              style: TextStyle(
+                fontSize: 23,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF4C53A5),
+              ),
+            ),
+          ),
+          Spacer(),
+          badges.Badge(
+            badgeContent: Text('e-commerce', style: TextStyle(color: Colors.red)),
+            child: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: null,
+            ),
+          ),
         ],
       ),
     );
